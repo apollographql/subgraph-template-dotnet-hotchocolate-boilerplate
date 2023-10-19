35b352d598e4286c572cfa357ed8938b6e917c21
Michael Watson <xambrew@gmail.com>
2023-10-19 14:51:23 -0700

    Router secret and Railway deployment

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
 using ApolloGraphQL.HotChocolate.Federation.Two;
+using HotChocolate.AspNetCore;
+using HotChocolate.Execution;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +13,29 @@ builder.Services
     .AddType<ContactDirectiveType>()
     .AddQueryType<Query>()
     .AddMutationType<Mutation>()
-    .RegisterService<Data>();
+    .RegisterService<Data>()
+    .AddHttpRequestInterceptor<RouterAuthInterceptor>();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://*:{port}");
 
 var app = builder.Build();
 
 app.MapGraphQL();
 app.MapBananaCakePop("/");
 app.Run();
+
+public sealed class RouterAuthInterceptor : DefaultHttpRequestInterceptor
+{
+    public override ValueTask OnCreateAsync(HttpContext context, IRequestExecutor requestExecutor, IQueryRequestBuilder requestBuilder, CancellationToken cancellationToken)
+    {
+        var incomingHeader = context.Request.Headers["router-authorization"];
+        var routerAuth = Environment.GetEnvironmentVariable("router-authorization");
+        if (routerAuth != null && incomingHeader != routerAuth)
+        {
+            throw new Exception("Missing router authentication");
+        }
+
+        return base.OnCreateAsync(context, requestExecutor, requestBuilder, cancellationToken);
+    }
+}
